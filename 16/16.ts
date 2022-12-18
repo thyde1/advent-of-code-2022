0532346025e6e1e3148807d630a6d1a199d5e82d
Tom Hyde <thomasdhyde@gmail.com>
2022-12-18 14:02:29 +0000

    Day 16 part 1

--- 16/16.ts
+++ 16/16.ts
@@ -1,4 +1,4 @@
-import { input } from "./sample";
+import { input } from "./input";
 
 type Valve = { id: string, flowRate: number, leadsToIds: string[] };
 
@@ -44,44 +44,26 @@ const getDistancesToOtherValves = (valves: Valve[]): ValveWithDistances[] => {
 }
 
 const getPermutations = (valves: ValveWithDistances[], time: number, startingValve: Valve): ValveWithDistances[][] => {
-    const eligibleValves = valves.filter(v => v.flowRate > 0 && v.distances[startingValve.id] < time - 1);
+    const eligibleValves = valves.filter(v => v.flowRate > 0 && v.distances[startingValve.id] < time);
 
-    if (eligibleValves.length === 1) {
+    if (eligibleValves.length <= 1) {
         return [eligibleValves];
     }
 
-    return eligibleValves.flatMap(v => 
+    return eligibleValves.flatMap(v =>
         getPermutations(
-            eligibleValves.filter(e => e !== v),
+            valves.filter(e => e !== v),
             time - v.distances[startingValve.id] - 1,
             v
         ).map(p => [v, ...p])
     );
 }
 
-const getNextMove = (valves: ValveWithDistances[], currentValve: ValveWithDistances, minutesRemaining: number) => {
-    const valvesWithBenefits = valves.map(v => ({
-        ...v,
-        benefit: (minutesRemaining - currentValve.distances[v.id] - 1) * v.flowRate,
-        distance: currentValve.distances[v.id]
-    }));
-
-    console.log(valvesWithBenefits.map(v => ({ id: v.id, ben: v.benefit, dist: v.distance })));
-    const adjustedValvesWithBenefits = valvesWithBenefits.map(v => ({
-        ...v,
-        benefit: v.benefit -
-            (valvesWithBenefits.filter(o => o.distance < v.distance).sort((a, b) => b.benefit - a.benefit)?.[0]?.benefit || 0)
-    }));
-    console.log(adjustedValvesWithBenefits.map(v => ({ id: v.id, benefit: v.benefit })));
-    return valvesWithBenefits.sort((a, b) => b.benefit - a.benefit)[0];
-};
-
 const getPermutationPressureReleased = (startingValveId: string, valves: ValveWithDistances[], time: number) => {
     let pressureReleased = 0;
     valves.forEach((v, i) => {
         time -= valves[i - 1]?.distances[v.id] || v.distances[startingValveId];
         time --;
-        console.log(time);
         pressureReleased += v.flowRate * time;
     });
 
@@ -92,10 +74,8 @@ const valves = getDistancesToOtherValves(getValves(input));
 const startingValveId = "AA";
 const possiblePermutations = getPermutations(valves, 30, valves.find(v => v.id === startingValveId) as ValveWithDistances);
 
-console.log(possiblePermutations.length);
 const bestPermutation = possiblePermutations.map(p => (
     { perm: p, pressure: getPermutationPressureReleased(startingValveId, p, 30) })
-).sort((a, b) => b.pressure - a.pressure)[0];
+    ).sort((a, b) => b.pressure - a.pressure)[0];
 
-console.log("BEST!", bestPermutation.pressure);
-getPermutationPressureReleased(startingValveId, bestPermutation.perm, 30);
+console.log(bestPermutation.pressure); // Answer to part 1
